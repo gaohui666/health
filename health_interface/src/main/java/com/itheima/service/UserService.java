422a13bae51c347c08f40263b344abb77b54ef56
itcast <hello@itcast>
2019-10-28 20:18:38 +0800

    新建方法

--- health_interface/src/main/java/com/itheima/service/UserService.java
+++ health_interface/src/main/java/com/itheima/service/UserService.java
@@ -2,7 +2,11 @@ package com.itheima.service;
 
 import com.itheima.pojo.User;
 
+import java.util.Map;
+
 public interface UserService {
 
     User findByUsername(String username);
+
+    Map<String, Object> loginByRole(String username);
 }
