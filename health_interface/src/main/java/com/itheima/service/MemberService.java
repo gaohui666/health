b69074dd05daa652d000c72ef591ea1f37633050
itcast <hello@itcast.cn>
2019-10-29 11:49:53 +0800

    折线图添加日期控件

--- health_interface/src/main/java/com/itheima/service/MemberService.java
+++ health_interface/src/main/java/com/itheima/service/MemberService.java
@@ -13,4 +13,5 @@ public interface MemberService {
     void add(Member member);
 
     List<Integer> findMemberCountByMonth(List<String> list);
+
 }
--- health_interface/src/main/java/com/itheima/service/ReportService.java
+++ health_interface/src/main/java/com/itheima/service/ReportService.java
@@ -1,5 +1,6 @@
 package com.itheima.service;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ReportService {
